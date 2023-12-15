2143c414fdce960bcf1811c99631971c87fd63c2
Мусатова Татьяна <MusatovaTF@yandex.ru>
2023-12-15 21:19:17 +0300

    Исправление - 1 ревью

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -1,14 +1,22 @@
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+RESULTS = 'results'
+(BASE_DIR / RESULTS).mkdir(exist_ok=True)
+
 BOT_NAME = 'pep_parse'
 
-SPIDER_MODULES = ['pep_parse.spiders']
+
 NEWSPIDER_MODULE = 'pep_parse.spiders'
+SPIDER_MODULES = [NEWSPIDER_MODULE, ]
 
 
 ROBOTSTXT_OBEY = True
 
 
 FEEDS = {
-    'results/pep_%(time)s.csv': {
+    f'{RESULTS}/pep_%(time)s.csv': {
         'format': 'csv',
         'fields': ['number', 'name', 'status'],
         'overwrite': True
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -6,22 +6,24 @@ from pep_parse.items import PepParseItem
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['https://peps.python.org/']
+    start_urls = [f'https://{domain}/' for domain in allowed_domains]
 
     def parse(self, response):
-        for tag in response.css('#numerical-index tr')[1:]:
+        for tag in response.css('#numerical-index tr')[1:].css(
+            'a::attr(href)'
+        ).getall():
             yield response.follow(
-                tag.css('a::attr(href)').get(), self.parse_pep
+                tag, self.parse_pep
             )
 
     def parse_pep(self, response):
-        title = response.css(
+        number, name = response.css(
             'head > title::text'
         ).get().split('|')[0].split(' – ')
-        yield PepParseItem({
-            'number': title[0].replace('PEP ', ''),
-            'name': title[1].strip(),
-            'status': response.css(
+        yield PepParseItem(
+            number=number.replace('PEP ', ''),
+            name=name.strip(),
+            status=response.css(
                 'dt:contains("Status") + dd'
             ).css('abbr::text').get()
-        })
+        )
